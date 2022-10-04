a79fae152719e0e625db835a921d61408036e8f5
Sebastian Berg Rasmussen <sr@scansolutions.dk>
2022-10-04 12:46:38 +0200

    Initial Commit

---
+++ src/test/java/com/gruppe1/kinoxp/KinoxpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruppe1.kinoxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoxpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
