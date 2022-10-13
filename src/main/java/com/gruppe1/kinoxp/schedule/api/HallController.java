de8545444a4b4d0b625bf55381d861baec4105a0
OmerMert32 <omermert@live.dk>
2022-10-13 12:52:37 +0200

    huskerikkeændringer

--- src/main/java/com/gruppe1/kinoxp/schedule/api/HallController.java
+++ src/main/java/com/gruppe1/kinoxp/schedule/api/HallController.java
@@ -1,18 +1,20 @@
 package com.gruppe1.kinoxp.schedule.api;
 
 
+import com.gruppe1.kinoxp.schedule.dto.response.EmployeeResponse;
 import com.gruppe1.kinoxp.schedule.dto.response.HallResponse;
+import com.gruppe1.kinoxp.schedule.dto.response.MovieResponse;
 import com.gruppe1.kinoxp.schedule.service.HallService;
+import com.gruppe1.kinoxp.schedule.service.MovieService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 @RestController
 @RequestMapping("api/v1/halls")
@@ -23,6 +25,9 @@ public class HallController {
 
     @Autowired
     HallService hallService;
+
+    @Autowired
+    MovieService movieService;
     @GetMapping
     public ResponseEntity<List<HallResponse>> getHalls(){
         return new ResponseEntity<>(hallService.getHalls(), HttpStatus.OK);
